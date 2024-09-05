415819b02ad0f50fbeb3be03e7d3673c867b653f
penguinerza <alghifarienerzas@gmail.com>
2024-09-06 03:52:24 +0700

    Updates

---
+++ _app/immutable/entry/start.B-ybeEvn.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Cp4uYVlo.js";export{t as start};
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{svelte,js,html}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
